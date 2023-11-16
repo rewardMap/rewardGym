f02b1cf847bc707ee969ce06ad46c7b5e32b6510
SR Steinkamp <simon.steinkamp@googlemail.com>
2023-11-16 10:38:54 +0100

    development setups

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import setup
+
+SETUP_REQUIRES = ["setuptools >= 30.3.0"]
+SETUP_REQUIRES += ["wheel"] if "bdist_wheel" in sys.argv else []
+
+if __name__ == "__main__":
+    setup(
+        name="rewardgym",
+        setup_requires=SETUP_REQUIRES,
+    )
