456000c961a07cc9394dbe73258cdd21934ab651
SRSteinkamp <simon.steinkamp@googlemail.com>
2024-04-08 13:58:45 +0200

    updated inits

--- rewardgym/__init__.py
+++ rewardgym/__init__.py
@@ -1,4 +1,4 @@
-from .tasks import get_env
+from .tasks import get_configs, get_env
 from .utils import unpack_conditions
 
 ENVIRONMENTS = ["hcp", "mid", "two-step", "risk-sensitive", "posner", "gonogo"]
--- rewardgym/tasks/__init__.py
+++ rewardgym/tasks/__init__.py
@@ -1,4 +1,4 @@
 from .hcp import get_hcp
 from .mid import get_mid
 from .two_step import get_two_step
-from .utils import get_env, get_task
+from .utils import get_configs, get_env, get_task
