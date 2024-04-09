253b82d8dc5ab024e3aa6a01340dff89ba26986c
SRSteinkamp <simon.steinkamp@googlemail.com>
2024-04-09 15:35:40 +0200

    polishing two-step

--- rewardgym/psychopy_render/two_step.py
+++ rewardgym/psychopy_render/two_step.py
@@ -9,16 +9,11 @@ total_reward_feedback = FeedBackText(
 )
 base_stim = BaseStimuli(1)
 fix = TextStimulus(text="+", duration=0.2)
+fix_iti = TextStimulus(text="+", duration=1.5, name="iti")
 
-image_shift = 400
+image_shift = 250
 
-final_step = [
-    base_stim,
-    reward_feedback,
-    total_reward_feedback,
-    BaseStimuli(duration=1.5, name="iti"),
-]
-
+final_step = [base_stim, reward_feedback, total_reward_feedback, fix_iti]
 
 info_dict = {
     0: {
@@ -40,6 +35,15 @@ info_dict = {
     1: {
         "psychopy": [
             ImageStimulus(
+                duration=0.5,
+                name="step1-select",
+                image_paths=[
+                    os.path.join(STIMPATH, "two_step", "stim11.png"),
+                    os.path.join(STIMPATH, "two_step", "stim12.png"),
+                ],
+                positions=[(0, image_shift), (image_shift, 0)],
+            ),
+            ImageStimulus(
                 duration=0.1,
                 name="step21",
                 image_paths=[
@@ -55,6 +59,15 @@ info_dict = {
     2: {
         "psychopy": [
             ImageStimulus(
+                duration=0.5,
+                name="step2-select",
+                image_paths=[
+                    os.path.join(STIMPATH, "two_step", "stim11.png"),
+                    os.path.join(STIMPATH, "two_step", "stim12.png"),
+                ],
+                positions=[(-image_shift, 0), (0, image_shift)],
+            ),
+            ImageStimulus(
                 duration=0.1,
                 name="step22",
                 image_paths=[
@@ -67,8 +80,44 @@ info_dict = {
             ActionStim(duration=2.0),
         ]
     },
-    3: {"psychopy": final_step},
-    4: {"psychopy": final_step},
-    5: {"psychopy": final_step},
-    6: {"psychopy": final_step},
+    3: {
+        "psychopy": [
+            ImageStimulus(
+                duration=0.5,
+                image_paths=[os.path.join(STIMPATH, "two_step", "stim21.png")],
+                positions=[(0, image_shift)],
+            )
+        ]
+        + final_step
+    },
+    4: {
+        "psychopy": [
+            ImageStimulus(
+                duration=0.5,
+                image_paths=[os.path.join(STIMPATH, "two_step", "stim22.png")],
+                positions=[(0, image_shift)],
+            )
+        ]
+        + final_step
+    },
+    5: {
+        "psychopy": [
+            ImageStimulus(
+                duration=0.5,
+                image_paths=[os.path.join(STIMPATH, "two_step", "stim31.png")],
+                positions=[(0, image_shift)],
+            )
+        ]
+        + final_step
+    },
+    6: {
+        "psychopy": [
+            ImageStimulus(
+                duration=0.5,
+                image_paths=[os.path.join(STIMPATH, "two_step", "stim32.png")],
+                positions=[(0, image_shift)],
+            )
+        ]
+        + final_step
+    },
 }
