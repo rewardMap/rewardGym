446e24a2ce9e802848a0a860f389044191842ede
SRSteinkamp <simon.steinkamp@googlemail.com>
2024-05-01 15:27:24 +0200

    implementing stubs for psychopy

---
+++ rewardgym/psychopy_render/psychopy_stubs.py
@@ -0,0 +1,36 @@
+class Clock:
+    def __init__(self):
+
+        self.time = 0
+
+    def getTime(self):
+
+        return self.time
+
+
+class Window:
+    def __init__(self, **kwargs):
+        pass
+
+    def flip(self):
+        pass
+
+    def close(self):
+        pass
+
+
+class ImageStim:
+    def __init__(self, *args, **kwargs) -> None:
+        pass
+
+
+class TextStim(ImageStim):
+    pass
+
+
+def getKeys(**kwargs):
+    pass
+
+
+def quit():
+    pass
