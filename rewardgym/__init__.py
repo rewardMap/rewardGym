2e61c5662475270ac1578ad660bb6074f9428cbe
SRSteinkamp <simon.steinkamp@googlemail.com>
2024-09-16 21:20:44 +0200

    improving importing

--- rewardgym/__init__.py
+++ rewardgym/__init__.py
@@ -1,6 +1,13 @@
+from .psychopy_render import get_psychopy_info
 from .tasks import get_configs, get_env
 from .utils import run_single_episode
 
 ENVIRONMENTS = ["hcp", "mid", "two-step", "risk-sensitive", "posner", "gonogo"]
 
-__all__ = ["get_configs", "get_env", "run_single_episode", "ENVIRONMENTS"]
+__all__ = [
+    "get_configs",
+    "get_env",
+    "run_single_episode",
+    "ENVIRONMENTS",
+    "get_psychopy_info",
+]
