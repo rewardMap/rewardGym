e49b5e6b5c4bc4867591bca0540c0c59d6817094
SRSteinkamp <simon.steinkamp@googlemail.com>
2024-09-25 12:57:48 +0200

    mid instructions uses frames now

--- assets/instructions/mid_instructions.py
+++ assets/instructions/mid_instructions.py
@@ -1,108 +1,93 @@
-import json
-
-from psychopy.event import waitKeys
 from psychopy.visual import ImageStim, TextBox2
 
 from rewardgym.psychopy_render.default_images import fixation_cross, mid_stimuli
 
 
-def mid_instructions(win, key_map={"left": 0, "right": 1}, show_training=True):
-    with open("assets/instructions/instructions_en.json") as f:
-        instructions = json.load(f)
-
-    instructions = instructions["mid"]
-    fix = fixation_cross()
-    key_list = list(key_map.keys())
-
-    part_0_0 = TextBox2(
-        win=win,
-        text=instructions["0.0"],
-        letterHeight=22,
-        pos=(0, 200),
-    )
-
-    stims = [
-        mid_stimuli(am, shap)
-        for am, shap in zip(
-            ["-5", "-1", "0", "+1", "+5"],
-            ["square", "square", "triangle_u", "circle", "circle"],
-        )
-    ]
-
-    left_pos = [-260, -135, 0, 135, 260]
-
+def mid_instructions():
     size = (125, 125)
 
-    stims = [
-        ImageStim(win=win, image=i, pos=(lp, 0), size=size)
-        for i, lp in zip(stims, left_pos)
-    ]
-    part_0_1 = TextBox2(
-        win=win,
-        text=instructions["0.1"],
-        letterHeight=22,
-        pos=(0, -200),
-    )
+    def part_0(win, instructions, size=size):
+        left_pos = [-260, -135, 0, 135, 260]
 
-    for ii in stims + [part_0_0, part_0_1]:
-        ii.draw()
+        part_0_0 = TextBox2(
+            win=win,
+            text=instructions["mid"]["0.0"],
+            letterHeight=24,
+            pos=(0, 200),
+        )
 
-    win.flip()
-    waitKeys()
+        stims = [
+            mid_stimuli(am, shap)
+            for am, shap in zip(
+                ["-5", "-1", "0", "+1", "+5"],
+                ["square", "square", "triangle_u", "circle", "circle"],
+            )
+        ]
 
-    part_1_0 = TextBox2(
-        win=win,
-        text=instructions["1.0"],
-        letterHeight=22,
-        pos=(0, 250),
-    )
+        stims = [
+            ImageStim(win=win, image=i, pos=(lp, 0), size=size)
+            for i, lp in zip(stims, left_pos)
+        ]
+        part_0_1 = TextBox2(
+            win=win,
+            text=instructions["mid"]["0.1"],
+            letterHeight=24,
+            pos=(0, -200),
+        )
 
-    fix = ImageStim(win=win, image=fix, size=fix.shape[:2])
+        for ii in stims + [part_0_0, part_0_1]:
+            ii.draw()
 
-    for i in [part_1_0, fix]:
-        i.draw()
+    def part_1(win, instructions):
+        fix = fixation_cross()
 
-    win.flip()
+        part_1_0 = TextBox2(
+            win=win,
+            text=instructions["mid"]["1.0"],
+            letterHeight=24,
+            pos=(0, 250),
+        )
 
-    waitKeys()
+        fix = ImageStim(win=win, image=fix, size=fix.shape[:2])
 
-    left_pos = [-130, 0, 130]
+        for i in [part_1_0, fix]:
+            i.draw()
 
-    stims = [
-        mid_stimuli(am, shap, probe=True)
-        for am, shap in zip(["", "", ""], ["square", "triangle_u", "circle"])
-    ]
+    def part_2(win, instructions, size=size):
+        left_pos = [-130, 0, 130]
 
-    stims = [
-        ImageStim(win=win, image=i, pos=(lp, 0), size=size)
-        for i, lp in zip(stims, left_pos)
-    ]
-    part_2_0 = TextBox2(
-        win=win,
-        text=instructions["2.0"],
-        letterHeight=22,
-        pos=(0, 200),
-    )
+        stims = [
+            mid_stimuli(am, shap, probe=True)
+            for am, shap in zip(["", "", ""], ["square", "triangle_u", "circle"])
+        ]
 
-    part_2_1 = TextBox2(
-        win=win,
-        text=instructions["2.1"],
-        letterHeight=22,
-        pos=(0, -200),
-    )
-    for ii in stims + [part_2_0, part_2_1]:
-        ii.draw()
+        stims = [
+            ImageStim(win=win, image=i, pos=(lp, 0), size=size)
+            for i, lp in zip(stims, left_pos)
+        ]
+        part_2_0 = TextBox2(
+            win=win,
+            text=instructions["mid"]["2.0"],
+            letterHeight=24,
+            pos=(0, 200),
+        )
 
-    win.flip()
-    waitKeys()
+        part_2_1 = TextBox2(
+            win=win,
+            text=instructions["mid"]["2.1"],
+            letterHeight=24,
+            pos=(0, -200),
+        )
+        for ii in stims + [part_2_0, part_2_1]:
+            ii.draw()
 
-    if show_training:
+    def part_3(win, instructions):
         part_3_0 = TextBox2(
-            win=win, text=instructions["3.0"], letterHeight=22, alignment="center"
+            win=win,
+            text=instructions["mid"]["3.0"],
+            letterHeight=24,
+            alignment="center",
         )
         part_3_0.draw()
-        win.flip()
 
-        waitKeys()
-
-    return key_list
+    return [part_0, part_1, part_2, part_3]
