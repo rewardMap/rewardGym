80fbeee6aa1749aaf5970b543e45bd675cc293ea
SRSteinkamp <simon.steinkamp@googlemail.com>
2024-09-26 13:49:25 +0200

    removing unused code, which is now in rwm_protocol

--- rewardgym/psychopy_extras.py
+++ rewardgym/psychopy_extras.py
@@ -1,90 +1,16 @@
 try:
     from psychopy.core import quit
-    from psychopy.event import getKeys
     from psychopy.gui import Dlg, DlgFromDict
-    from psychopy.visual import ImageStim, TextStim
 except ModuleNotFoundError:
     from .psychopy_render.psychopy_stubs import (
-        TextStim,
-        ImageStim,
         DlgFromDict,
         Dlg,
-        getKeys,
         quit,
     )
 
 import os
 
-import matplotlib.pyplot as plt
-
 from . import ENVIRONMENTS
-
-
-def show_instructions(
-    win,
-    image_path,
-    view_height=500,
-    scroll_speed=50,
-    width_scale=1.0,
-    key_map=["left", "right", "down"],
-    mode="behavior",
-):
-    image = plt.imread(image_path)[::-1, :, :]
-
-    img_shape = image.shape
-    y_pos = img_shape[0] - view_height
-
-    button = ImageStim(
-        win, image="assets/instructions/buttonbox.png", pos=(0, -view_height // 2 - 50)
-    )
-    button.setAutoDraw(True)
-
-    if mode == "behavior":
-        press_button = key_map[-1]
-    elif mode == "fmri":
-        press_button = "\n the third button (ring finger)."
-
-    text = TextStim(
-        win,
-        text=f"To begin the task press {press_button}",
-        pos=(0, -view_height // 2 - 100),
-        height=20,
-    )
-
-    trigger = False
-
-    while True:
-        # Draw the image
-        img = ImageStim(
-            win,
-            image=image[y_pos : view_height + y_pos, :],
-            size=(int(img_shape[1] * width_scale), view_height),
-        )
-        img.draw()
-        win.flip()
-
-        # Check for keypresses
-        keys = getKeys()
-
-        if key_map[0] in keys:
-            if (y_pos + view_height + scroll_speed) <= img_shape[0]:
-                y_pos += scroll_speed
-
-        elif key_map[1] in keys:
-            if (y_pos - scroll_speed) >= 0:
-                y_pos -= scroll_speed
-            else:
-                trigger = True
-
-        elif key_map[2] in keys:
-            # Exit the loop
-            break
-
-        if trigger:
-            text.draw()
-
-    button.setAutoDraw(False)
-    win.flip()
 
 
 def set_up_experiment(outdir="data/"):
