8cb205d5c8b5610995c756b8bedf24529583fb30
SRSteinkamp <simon.steinkamp@googlemail.com>
2025-01-23 13:23:08 +0100

    remap actions are not present anymore.

--- rewardgym/psychopy_core.py
+++ rewardgym/psychopy_core.py
@@ -167,9 +167,7 @@ def run_task(
                     done = draw_response_reminder(win, response_reminder, logger)
 
             elif not (done and env.previous_action is None):
-                agent.update(
-                    obs, env.previous_remap_action, reward, terminated, next_obs
-                )
+                agent.update(obs, env.previous_action, reward, terminated, next_obs)
                 obs = next_obs
 
         # TODO: Also a different function?
