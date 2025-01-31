d721d2dfff60a356d615331764e06f9e44d007b3
SRSteinkamp <simon.steinkamp@googlemail.com>
2025-01-31 11:47:43 +0100

    Changing max points for gonogo to be only 80 % of best expected outcome

--- rewardgym/tasks/__init__.py
+++ rewardgym/tasks/__init__.py
@@ -6,7 +6,7 @@ FULLPOINTS = {
     "hcp": 13.5,
     "risk-sensitive": 5150,
     "two-step": 100,
-    "gonogo": 50,
+    "gonogo": 43,
 }
 
 __all__ = ["get_configs", "get_env", "get_task", "FULLPOINTS"]
