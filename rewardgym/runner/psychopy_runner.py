a7edd054af6a7bee7c8fc4f7d7bb4f14d55f7da0
SRSteinkamp <simon.steinkamp@googlemail.com>
2025-06-16 13:06:03 +0200

    plug-ins written (first version)

--- rewardgym/runner/psychopy_runner.py
+++ rewardgym/runner/psychopy_runner.py
@@ -3,12 +3,107 @@ try:
 except ModuleNotFoundError:
     from ..psychopy_render.psychopy_stubs import TextStim, Window
 
+from typing import Dict
+
 import numpy as np
 
 from ..psychopy_render.psychopy_stubs import Window
 from ..tasks.utils import get_configs
 from .psychopy_plugins import break_point, draw_response_reminder, simple_rt_simulation
 from .psychopy_utils import update_psychopy_trials
+
+
+class PsyPyPlugin:
+    def __init__(self):
+        pass
+
+    def modify(self, **kwargs):
+        pass
+
+
+class MiscUpdateMid(PsyPyPlugin):
+    def modify(self, *, env, logger, **kwargs):
+        misc = env.info_dict[1]["psychopy"][-1].duration
+        logger.update_trial_info(
+            misc=misc,
+        )
+        return None
+
+
+class MiscUpdateTwoStep(PsyPyPlugin):
+    def modify(self, *, env, logger, **kwargs):
+        misc = [round(ii.p, 5) for ii in env.reward_locations.values()]
+        logger.update_trial_info(
+            misc=misc,
+        )
+        return None
+
+
+class TimeUpdateMid(PsyPyPlugin):
+    def modify(self, *, episode, settings, env, **kwargs):
+        settings["reward"][episode] = (
+            settings["reward"][episode] - env.info_dict[1]["psychopy"][-1].duration
+        )
+
+        return None
+
+
+class DelayUpdateHcp(PsyPyPlugin):
+    def modify(self, *, settings, env, episode, **kwargs):
+        settings["delay"][episode] = env.remainder
+        update_psychopy_trials(settings, env, episode)
+
+        return None
+
+
+class StairCaseMid(PsyPyPlugin):
+    def __init__(self, trial_counter=0):
+        self.trial_counter = trial_counter
+
+    def modify(self, *, settings, episode, env, actions, **kwargs):
+        if settings["condition"][episode] in ["win-large", "win-small"]:
+            self.trial_counter += 1
+
+            if (self.trial_counter % 3) == 0:
+                adjustment = (
+                    -0.025 if (np.nansum(actions) / (episode + 1)) < 0.4 else 0.025
+                )
+            else:
+                adjustment = 0
+
+            # Duration is at minimum 150 ms and at max 500 ms, need to only update first occurence
+            # of first stimulus, as it's reused
+            new_duration = env.info_dict[1]["psychopy"][-1].duration + adjustment
+            new_duration = np.max([np.min([new_duration, 0.5]), 0.20])
+            env.info_dict[1]["psychopy"][-1].duration = new_duration
+
+
+class AddRemainder(PsyPyPlugin):
+    def modify(self, *, env, settings, win, logger, **kwargs):
+        if env.remainder > 0 and settings["add_remainder"]:
+            rm_onset = logger.get_time()
+            logger.wait(win, env.remainder, rm_onset)
+
+            logger.log_event(
+                {"event_type": "adjusting-time", "expected_duration": env.remainder},
+                onset=rm_onset,
+            )
+
+
+# Plugins use different entrypoints:
+
+{
+    "pre-trial": [],
+    "after-action": [],
+    "post-trial": [],
+}
+
+plugin_registry = {
+    "mid": {
+        "pre-trial": [MiscUpdateMid(), TimeUpdateMid()],
+        "post-trial": [AddRemainder(), StairCaseMid(0)],
+    }
+}
 
 
 def pspy_run_task(
@@ -19,6 +114,7 @@ def pspy_run_task(
     seed=111,
     agent=None,
     n_episodes=None,
+    plugins: Dict = None,
 ):
     if settings is None:
         settings = get_configs(env.name)(seed)
@@ -54,6 +150,13 @@ def pspy_run_task(
 
     for episode in range(n_episodes):
         done = False
+
+        logger.update_trial_info(
+            trial=episode,
+            start_position=0,
+            misc="n/a",
+        )
+
         # Update timings
         if env.name == "mid":
             settings["reward"][episode] = (
@@ -62,17 +165,13 @@ def pspy_run_task(
             misc = env.info_dict[1]["psychopy"][-1].duration
         elif env.name == "two-step":
             misc = [round(ii.p, 5) for ii in env.reward_locations.values()]
-        else:
-            misc = "n/a"
 
+        print(misc)  # Going to remove in next commit
         update_psychopy_trials(settings, env, episode)
 
         logger.set_trial_time()
         logger.update_trial_info(
             trial_type=settings["condition"][episode],
-            trial=episode,
-            start_position=0,
-            misc=misc,
         )
 
         obs, info = env.reset(
