9ba8513e4d3d011f3001dcac2eafee115fd3c044
SRSteinkamp <simon.steinkamp@googlemail.com>
2025-08-06 17:16:12 +0200

    init now dynamically defines ENVIRONENMENT variable

--- rewardgym/__init__.py
+++ rewardgym/__init__.py
@@ -1,18 +1,14 @@
+import pathlib
+
 from . import _version
 from .tasks import get_configs, get_env, get_psychopy_info, task_loader
-from .utils import run_single_episode
+from .utils import check_seed, run_single_episode
 
 _task_registry = task_loader._discover_plugins()
 
+ASSETS_PATH = pathlib.Path(__file__).parent.resolve() / "assets"
 
-ENVIRONMENTS = [
-    "hcp",
-    "mid",
-    "two-step",
-    "risk-sensitive",
-    "posner",
-    "gonogo",
-]
+ENVIRONMENTS = list(_task_registry.keys())
 
 __all__ = [
     "get_configs",
@@ -20,6 +16,7 @@ __all__ = [
     "run_single_episode",
     "ENVIRONMENTS",
     "get_psychopy_info",
+    "check_seed",
 ]
 
 __version__ = _version.get_versions()["version"]
